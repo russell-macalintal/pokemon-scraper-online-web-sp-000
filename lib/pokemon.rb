5699651bf04a4a026ac9fe61fde97a39548f20b7
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-04 05:18:29 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(:id => nil, :name, :type, :db)
+  def initialize(id: nil, name:, type:, db:)
     @id = :id
     @name = :name
     @type = :type
