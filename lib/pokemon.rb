030599deb2bd69c6c6bfb0bc248bc03adde23452
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-04 05:29:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,7 +26,7 @@ class Pokemon
 
     sample = db.execute(sql, id).collect do |pokemon|
       # binding.pry
-      Pokemon.new(pokemon[0], pokemon[1], pokemon[2], db)
+      Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
     end.first
     binding.pry
   end
