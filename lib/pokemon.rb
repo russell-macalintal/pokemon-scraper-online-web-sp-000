c1a5e55a8571ea221df0b7ea8cca906c1ed3dd1e
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-04 05:31:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    # pokemon = self.new(name: name, type: type, db: db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (? , ?)
+      SQL
+
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?
+      SQL
+
+    db.execute(sql, id).collect do |pokemon|
+      Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end.first
+  end
+
 end
