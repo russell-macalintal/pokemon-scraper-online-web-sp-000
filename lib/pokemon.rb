b8b90de0605a3eb79b3e978b848e02856aacad08
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-04 04:45:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @id = :id
+    @name = :name
+    @type = :type
+    @db = :db
+  end
+
+  def self.save
+  end
+
+  def self.find
+  end
+
 end
